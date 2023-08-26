de61ca5a9ecd6db0d1aea214e24ea03a0d82798d
demch <offer.demchenko@gmail.com>
2023-08-26 12:38:18 +0300

    finish model Item

--- food_place/item/models.py
+++ food_place/item/models.py
@@ -5,27 +5,25 @@ from django.utils.text import slugify
 # Create your models here.
 
 
-
-
-
+# class Item create for main information of products
 class Item(models.Model):
-        name = models.CharField(max_length=150)
-        type = models.CharField(max_length=150)
-        description = models.TextField(blank=True)
-        price = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=150)
+    type = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
-        def upload_to(instance, filename):
-                return f"media/{instance.type}/{filename}"
+# upload_to create name-based path to images
+    def upload_to(instance, filename):
+        return f"media/{instance.type}/{filename}"
 
-        image = models.ImageField(upload_to=upload_to)
+    image = models.ImageField(upload_to=upload_to)
 
+    is_available = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True)
 
-        is_available = models.BooleanField(default=False)
-        slug = models.SlugField(unique=True)
-
-        def save(self, *args, **kwargs):
-                if not self.description:
-                        self.description = f"This is a {self.type} named {self.name}."
-                self.slug = slugify(self.name)
-                super().save(*args, **kwargs)
-
+# auto-creating slug and description for products
+    def save(self, *args, **kwargs):
+        if not self.description:
+            self.description = f"This is a {self.type} named {self.name}."
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
