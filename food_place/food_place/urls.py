2796fdb59bb16625221f446a54a1f9c16631b269
demch <offer.demchenko@gmail.com>
2023-08-26 11:56:21 +0300

    basic structure. config media root. create model Item.

---
+++ food_place/food_place/urls.py
@@ -0,0 +1,46 @@
+"""
+URL configuration for food_place project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls.static import static
+
+from comments.views import *
+from common.views import *
+from core.views import *
+from feedback.views import *
+from food_place import settings
+from item.views import *
+from order.views import *
+from transaction.views import *
+from user.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('comments/', include('comments.urls')),
+    path('common/', include('common.urls')),
+    path('core/', include('core.urls')),
+    path('feedback/', include('feedback.urls')),
+    path('item/', include('item.urls')),
+    path('order/', include('order.urls')),
+    path('transaction/', include('transaction.urls')),
+    path('user/', include('user.urls')),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
